78da22719e05d1e5684f63f23f34b434cb14c3fc
ishihara1989 <ashitahamatsuri@gmail.com>
2017-10-31 00:53:09 +0900

    use inspect

--- cupy2implement.py
+++ cupy2implement.py
@@ -1,10 +1,15 @@
+import inspect
+
 import cupy
 import numpy as np
 
 
 if __name__ == '__main__':
-    np_symbols = dir(np)
-    cupy_symbols = dir(cupy)
+    np_symbols = dict(inspect.getmembers(np))
+    cupy_symbols = dict(inspect.getmembers(cupy))
+    functions = []
+    modules = []
+    others = []
     for sym in np_symbols:
         if sym in cupy_symbols:
             continue
@@ -13,4 +18,15 @@ if __name__ == '__main__':
         if sym[0].isupper():
             continue
 
-        print(sym)
+        if inspect.ismodule(np_symbols[sym]):
+            modules.append(sym)
+        elif inspect.isfunction(np_symbols[sym]):
+            functions.append(sym)
+        else:
+            others.append(sym)
+
+    print("others: ", others)
+    print("-"*70)
+    print("modules: ", modules)
+    print("-"*70)
+    print("functions ", functions)
